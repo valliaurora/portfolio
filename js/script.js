6be5331ebe252c1f22e5f34ae4fb5528210264de
valliaurora <105985732+valliaurora@users.noreply.github.com>
2024-07-04 18:32:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,432 @@
+/*
+* ----------------------------------------------------------------------------------------
+Author       : Hridoy
+Template Name: Kane - Personal Portfolio HTML Template
+Version      : 1.0                                          
+* ----------------------------------------------------------------------------------------
+*/
+
+
+(function($) {
+
+    "use strict";
+
+    $(document).ready(function() {
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  EXTRA JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('.nav-link-click').click(function() {
+            $('.navbar-collapse').collapse('hide');
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PRELOADER JS & DOCUMENT LOAD JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(window).on('load', function() {
+
+            $('.loader').fadeOut();
+            $('#preloader-area').delay(350).fadeOut('slow');
+
+
+            // ## Project Filtering
+            if ($('.project-masonry-active').length) {
+                $(this).imagesLoaded(function() {
+                    $('.project-masonry-active').isotope({
+                        // options
+                        itemSelector: '.item',
+                    });
+                });
+            }
+
+
+            // ## Blog Standard
+            if ($('.blog-standard-wrap').length) {
+                $(this).imagesLoaded(function() {
+                    $('.blog-standard-wrap').isotope({
+                        // options
+                        itemSelector: '.item',
+                    });
+                });
+            }
+
+
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  HEADER STYLE JS
+         * ----------------------------------------------------------------------------------------
+         */
+        function headerStyle() {
+            if ($('.main-header').length) {
+                var windowpos = $(window).scrollTop();
+                var siteHeader = $('.main-header');
+                var scrollLink = $('.scroll-top');
+                if (windowpos >= 250) {
+                    siteHeader.addClass('fixed-header');
+                    scrollLink.fadeIn(300);
+                } else {
+                    siteHeader.removeClass('fixed-header');
+                    scrollLink.fadeOut(300);
+                }
+            }
+        }
+        headerStyle();
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  MAGNIFIC POPUP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var magnifPopup = function() {
+            $('.work-popup').magnificPopup({
+                type: 'image',
+                removalDelay: 300,
+                mainClass: 'mfp-with-zoom',
+                gallery: {
+                    enabled: true
+                },
+                zoom: {
+                    enabled: false, // By default it's false, so don't forget to enable it
+
+                    duration: 300, // duration of the effect, in milliseconds
+                    easing: 'ease-in-out', // CSS transition easing function
+
+                    // The "opener" function should return the element from which popup will be zoomed in
+                    // and to which popup will be scaled down
+                    // By defailt it looks for an image tag:
+                    opener: function(openerElement) {
+                        // openerElement is the element on which popup was initialized, in this case its <a> tag
+                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                        return openerElement.is('img') ? openerElement : openerElement.find('img');
+                    }
+                }
+            });
+
+
+            $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+                disableOn: 700,
+                type: 'iframe',
+                mainClass: 'mfp-fade',
+                removalDelay: 160,
+                preloader: false,
+
+                fixedContentPos: false
+            });
+
+        };
+        // Call the functions 
+        magnifPopup();
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SCROOL TO UP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var progressPath = document.querySelector('.progress-wrap path');
+        var pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+        var updateProgress = function() {
+            var scroll = $(window).scrollTop();
+            var height = $(document).height() - $(window).height();
+            var progress = pathLength - (scroll * pathLength / height);
+            progressPath.style.strokeDashoffset = progress;
+        }
+        updateProgress();
+        $(window).scroll(updateProgress);
+        var offset = 150;
+        var duration = 550;
+        jQuery(window).on('scroll', function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.progress-wrap').addClass('active-progress');
+            } else {
+                jQuery('.progress-wrap').removeClass('active-progress');
+            }
+        });
+        jQuery('.progress-wrap').on('click', function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({
+                scrollTop: 0
+            }, duration);
+            return false;
+        })
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  DROPDOWN MENU JS
+         * ----------------------------------------------------------------------------------------
+         */
+        var mobileWidth = 992;
+        var navcollapse = $('.navigation li.dropdown');
+
+        navcollapse.hover(function() {
+            if ($(window).innerWidth() >= mobileWidth) {
+                $(this).children('ul').stop(true, false, true).slideToggle(300);
+                $(this).children('.megamenu').stop(true, false, true).slideToggle(300);
+            }
+        });
+
+        // ## Submenu Dropdown Toggle
+        if ($('.main-header .navigation li.dropdown ul').length) {
+            $('.main-header .navigation li.dropdown').append('<div class="dropdown-btn"><span class="fas fa-chevron-down"></span></div>');
+
+            //Dropdown Button
+            $('.main-header .navigation li.dropdown .dropdown-btn').on('click', function() {
+                $(this).prev('ul').slideToggle(500);
+                $(this).prev('.megamenu').slideToggle(800);
+            });
+
+            //Disable dropdown parent link
+            $('.navigation li.dropdown > a').on('click', function(e) {
+                e.preventDefault();
+            });
+        }
+
+        // Submenu Dropdown Toggle
+        if ($('.main-header .main-menu').length) {
+            $('.main-header .main-menu .navbar-toggle').click(function() {
+                $(this).prev().prev().next().next().children('li.dropdown').hide();
+            });
+        }
+
+
+
+
+        // ## OnePage Nav Scroll
+        $(".onepage a").on('click', function(e) {
+            e.preventDefault();
+            var hash = this.hash;
+            var position = $(hash).offset().top;
+            $("html").animate({
+                scrollTop: position
+            }, 1000);
+        });
+
+
+        // ## Testimonials Active
+        if ($('.testimonials-wrap').length) {
+            $('.testimonials-wrap').slick({
+                dots: false,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                arrows: true,
+                speed: 1000,
+                focusOnSelect: false,
+                prevArrow: '.testimonial-prev',
+                nextArrow: '.testimonial-next',
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                responsive: [{
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }]
+            });
+        }
+
+
+
+        // ## Project Filter
+        $(".project-filter li").on('click', function() {
+            $(".project-filter li").removeClass("current");
+            $(this).addClass("current");
+
+            var selector = $(this).attr('data-filter');
+            $('.project-masonry-active').imagesLoaded(function() {
+                $(".project-masonry-active").isotope({
+                    itemSelector: '.item',
+                    filter: selector,
+                    masonry: {
+                        columnWidth: '.item'
+                    }
+                });
+            });
+
+        });
+
+
+
+        /* ## Fact Counter + Text Count - Our Success */
+        if ($('.counter-text-wrap').length) {
+            $('.counter-text-wrap').appear(function() {
+
+                var $t = $(this),
+                    n = $t.find(".count-text").attr("data-stop"),
+                    r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+
+                if (!$t.hasClass("counted")) {
+                    $t.addClass("counted");
+                    $({
+                        countNum: $t.find(".count-text").text()
+                    }).animate({
+                        countNum: n
+                    }, {
+                        duration: r,
+                        easing: "linear",
+                        step: function() {
+                            $t.find(".count-text").text(Math.floor(this.countNum));
+                        },
+                        complete: function() {
+                            $t.find(".count-text").text(this.countNum);
+                        }
+                    });
+                }
+
+            }, {
+                accY: 0
+            });
+        }
+
+
+
+        // ## Scroll to Top
+        if ($('.scroll-to-target').length) {
+            $(".scroll-to-target").on('click', function() {
+                var target = $(this).attr('data-target');
+                // animate
+                $('html, body').animate({
+                    scrollTop: $(target).offset().top
+                }, 1000);
+
+            });
+        }
+
+
+        // ## Nice Select
+        $('select').niceSelect();
+
+
+        // ## WOW Animation
+        if ($('.wow').length) {
+            var wow = new WOW({
+                boxClass: 'wow', // animated element css class (default is wow)
+                animateClass: 'animated', // animation css class (default is animated)
+                offset: 0, // distance to the element when triggering the animation (default is 0)
+                mobile: false, // trigger animations on mobile devices (default is true)
+                live: true // act on asynchronously loaded content (default is true)
+            });
+            wow.init();
+        }
+
+
+    });
+
+
+    /* ==========================================================================
+       When document is resize, do
+       ========================================================================== */
+
+    $(window).on('resize', function() {
+        var mobileWidth = 992;
+        var navcollapse = $('.navigation li.dropdown');
+        navcollapse.children('ul').hide();
+        navcollapse.children('.megamenu').hide();
+
+    });
+
+
+/*
+     * ----------------------------------------------------------------------------------------
+     *  AJAX CONTACT JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    // Function for email address validation
+    function isValidEmail(emailAddress) {
+        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+        return pattern.test(emailAddress);
+
+    }
+    $("#contactForm").on('submit', function(e) {
+        e.preventDefault();
+        var data = {
+            name: $("#name").val(),
+            email: $("#email").val(),
+            subject: $("#subject").val(),
+            message: $("#message").val()
+        };
+
+        if (isValidEmail(data['email']) && (data['message'].length > 1) && (data['name'].length > 1) && (data['subject'].length > 1)) {
+            $.ajax({
+                type: "POST",
+                url: "sendmail.php",
+                data: data,
+                success: function() {
+                    $('#contactForm .input-success').delay(500).fadeIn(1000);
+                    $('#contactForm .input-error').fadeOut(500);
+                }
+            });
+        } else {
+            $('#contactForm .input-error').delay(500).fadeIn(1000);
+            $('#contactForm .input-success').fadeOut(500);
+        }
+
+        return false;
+    });
+
+
+    /* ==========================================================================
+       When document is scroll, do
+       ========================================================================== */
+
+    $(window).on('scroll', function() {
+
+        // ## Header Style and Scroll to Top
+        function headerStyle() {
+            if ($('.main-header').length) {
+                var windowpos = $(window).scrollTop();
+                var siteHeader = $('.main-header');
+                var scrollLink = $('.scroll-top');
+                if (windowpos >= 100) {
+                    siteHeader.addClass('fixed-header');
+                    scrollLink.fadeIn(300);
+                } else {
+                    siteHeader.removeClass('fixed-header');
+                    scrollLink.fadeOut(300);
+                }
+            }
+        }
+
+        headerStyle();
+
+    });
+
+    /* ==========================================================================
+       When document is loaded, do
+       ========================================================================== */
+
+    $(window).on('load', function() {
+
+        $('.loader').fadeOut();
+        $('#preloader-area').delay(350).fadeOut('slow');
+
+
+
+
+
+
+    });
+
+})(window.jQuery);
